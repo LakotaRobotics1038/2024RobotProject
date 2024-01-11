6bb0f7626cda675850605b7db32dfd3e5863867b
Maryam A. Tunkara <tunkarama@1038.org>
2024-01-10 19:43:34 -0500

    made scoring subsystem file

---
+++ src/main/java/frc/robot/subsystems/Scoring.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+public class Scoring extends SubsystemBase {
+
+}

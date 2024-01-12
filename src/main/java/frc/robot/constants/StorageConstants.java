2d2c123c6bce949efd1f37a165f6755749828cd4
Griffin M. Lamb <lambgm@1038.org>
2024-01-11 19:48:03 -0500

    finished the issue for storage subsystem skeleton

---
+++ src/main/java/frc/robot/constants/StorageConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public class StorageConstants {
+
+    public static final int kStorageMotorPort = 0;
+}
---
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.StorageConstants;
+
+public class Storage extends SubsystemBase {
+
+    public final CANSparkMax storageMotor = new CANSparkMax(StorageConstants.kStorageMotorPort,
+            MotorType.kBrushless);
+
+    private static Storage instance;
+
+    public static Storage getInstance() {
+        if (instance == null) {
+            instance = new Storage();
+        }
+        return instance;
+    }
+
+    private Storage() {
+        storageMotor.restoreFactoryDefaults();
+        storageMotor.setInverted(true);
+        storageMotor.setIdleMode(IdleMode.kBrake);
+        storageMotor.burnFlash();
+    }
+
+    public void runStorage(double speed) {
+        storageMotor.set(speed);
+    }
+
+    public void stopStorage() {
+        storageMotor.stopMotor();
+    }
+}

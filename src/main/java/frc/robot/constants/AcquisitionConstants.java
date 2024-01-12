767739c3a8a861a390290fc030e81d0ddd08183d
Samuel S. Faw <fawss@1038.org>
2024-01-11 19:47:04 -0500

    finished issue 1

---
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+public class AcquisitionConstants {
+    public static final int rearMotorID = -1;
+    public static final int frontMotorID = -1;
+
+    public static final double rearMotorSpeed = 0.0;
+    public static final double frontMotorSpeed = 0.0;
+}
---
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.AcquisitionConstants;
+
+public class Acquisition extends SubsystemBase {
+    private static Acquisition instance;
+
+    private static CANSparkMax rearMotor = new CANSparkMax(AcquisitionConstants.frontMotorID, MotorType.kBrushless);
+    private static CANSparkMax frontMotor = new CANSparkMax(AcquisitionConstants.frontMotorID, MotorType.kBrushless);
+
+    private Acquisition() {
+        rearMotor.restoreFactoryDefaults();
+        frontMotor.restoreFactoryDefaults();
+        rearMotor.setIdleMode(IdleMode.kCoast);
+        frontMotor.setIdleMode(IdleMode.kCoast);
+        rearMotor.burnFlash();
+        frontMotor.burnFlash();
+    }
+
+    public static Acquisition getInstance() {
+        if (instance == null) {
+            instance = new Acquisition();
+        }
+        return instance;
+    }
+
+    public void runRear() {
+        rearMotor.set(AcquisitionConstants.rearMotorSpeed);
+    }
+
+    public void runFront() {
+        frontMotor.set(AcquisitionConstants.frontMotorSpeed);
+    }
+
+    public void stopRear() {
+        rearMotor.stopMotor();
+    }
+
+    public void stopFront() {
+        frontMotor.stopMotor();
+    }
+}

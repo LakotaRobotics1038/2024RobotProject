4a49d89763a02389760062013c6d34d5ac4ed1f2
matunkar <122322635+matunkar@users.noreply.github.com>
2024-01-13 12:57:12 -0500

    Update src/main/java/frc/robot/constants/LiftConstants.java
    Co-authored-by: Wesley Reed <24629474+reediculous456@users.noreply.github.com>

--- src/main/java/frc/robot/constants/LiftConstants.java
+++ src/main/java/frc/robot/constants/LiftConstants.java
@@ -1,6 +1,6 @@
 public class LiftConstants {
-    public static final int leftLiftMotorPort = 0;
-    public static final int rightLiftMotorPort = 1;
+    public static final int leftMotorPort = 0;
+    public static final int rightMotorPort = 1;
 
     public static final double motorSpeed = 1;
 }

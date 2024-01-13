fc4689ab3927a3c8658e00afa554280bc041114b
Griffin M. Lamb <lambgm@1038.org>
2024-01-13 09:26:33 -0500

    made scoring.java a singleton

--- src/main/java/frc/robot/subsystems/Scoring.java
+++ src/main/java/frc/robot/subsystems/Scoring.java
@@ -1,10 +1,21 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMax.IdleMode;
+//import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//import com.revrobotics.CANSparkMax.ControlType;
+//import com.revrobotics.CANSparkMax.IdleMode;
 
 public class Scoring extends SubsystemBase {
 
+    private static Scoring instance;
+
+    public static Scoring getInstance() {
+        if (instance == null) {
+            instance = new Scoring();
+        }
+        return instance;
+    }
 }

ea1e53af69aeb2ec81555364e3cadfec15292f20
Griffin M. Lamb <lambgm@1038.org>
2024-01-13 11:55:21 -0500

    finished with skeleton for scoring

---
+++ src/main/java/frc/robot/constants/ScoringConstants.java
@@ -0,0 +1,17 @@
+package frc.robot.constants;
+
+public class ScoringConstants {
+    public static final int kLeftScoringElevatorMotorPort = 0;
+    public static final int kRightScoringElevatorMotorPort = 0;
+    public static final int kRollerMotorPort = 0;
+
+    public static final double kGroundSetpoint = 0;
+    public static final double kAmpSetpoint = 0;
+    public static final double kTrapSetpoint = 0;
+
+    public static final double kMaxDistance = 0;
+
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+}
--- src/main/java/frc/robot/subsystems/Scoring.java
+++ src/main/java/frc/robot/subsystems/Scoring.java
@@ -1,21 +1,103 @@
 package frc.robot.subsystems;
 
-//import com.revrobotics.CANSparkMax;
-//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.AbsoluteEncoder;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ScoringConstants;
 
-//import com.revrobotics.CANSparkMax.ControlType;
-//import com.revrobotics.CANSparkMax.IdleMode;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
-public class Scoring extends SubsystemBase {
+import edu.wpi.first.math.MathUtil;
+
+public class Scoring extends PIDSubsystem {
+
+    public final CANSparkMax leftScoringElevatorMotor = new CANSparkMax(ScoringConstants.kLeftScoringElevatorMotorPort,
+            MotorType.kBrushless);
+    private final CANSparkMax rightScoringElevatorMotor = new CANSparkMax(
+            ScoringConstants.kRightScoringElevatorMotorPort, MotorType.kBrushless);
+    private final CANSparkMax rollerMotor = new CANSparkMax(
+            ScoringConstants.kRollerMotorPort, MotorType.kBrushless);
+
+    private AbsoluteEncoder leftScoringElevatorEncoder = leftScoringElevatorMotor.getAbsoluteEncoder(Type.kDutyCycle);
+
+    public enum ElevatorSetpoints {
+        ground(ScoringConstants.kGroundSetpoint),
+        amp(ScoringConstants.kAmpSetpoint),
+        trap(ScoringConstants.kTrapSetpoint);
+
+        public final double value;
+
+        ElevatorSetpoints(double value) {
+            this.value = value;
+        }
+    }
 
     private static Scoring instance;
 
     public static Scoring getInstance() {
+
         if (instance == null) {
             instance = new Scoring();
         }
         return instance;
+    }
+
+    public Scoring() {
+        super(new PIDController(ScoringConstants.kP, ScoringConstants.kI, ScoringConstants.kD));
+        rightScoringElevatorMotor.restoreFactoryDefaults();
+        leftScoringElevatorMotor.restoreFactoryDefaults();
+        rightScoringElevatorMotor.setIdleMode(IdleMode.kCoast);
+        leftScoringElevatorMotor.setIdleMode(IdleMode.kCoast);
+        rightScoringElevatorMotor.follow(leftScoringElevatorMotor);
+        rightScoringElevatorMotor.burnFlash();
+        leftScoringElevatorMotor.burnFlash();
+    }
+
+    @Override
+    protected void useOutput(double output, double setpoint) {
+
+    }
+
+    @Override
+    protected double getMeasurement() {
+        return getPosition();
+    }
+
+    public void setClampSetpoint(double setpoint) {
+        setpoint = MathUtil.clamp(setpoint, 0, ScoringConstants.kMaxDistance);
+        super.setSetpoint(setpoint);
+    }
+
+    public void setSetpoint(ElevatorSetpoints setpoint) {
+        setSetpoint(setpoint.value);
+    }
+
+    public double getPosition() {
+        return leftScoringElevatorEncoder.getPosition();
+    }
+
+    public void runRoller(double speed) {
+        rollerMotor.set(speed);
+    }
+
+    public void stopRoller() {
+        rollerMotor.stopMotor();
+    }
+
+    public void setP(double p) {
+        getController().setP(p);
+    }
+
+    public void setI(double i) {
+        getController().setI(i);
+    }
+
+    public void setD(double d) {
+        getController().setD(d);
     }
 }

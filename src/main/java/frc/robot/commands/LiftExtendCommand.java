10cfa5738bd1a401799760842a6b15af2434324a
Griffin M. Lamb <lambgm@1038.org>
2024-01-15 11:20:30 -0500

    updated name of file

--- src/main/java/frc/robot/commands/LiftExtendCommand.java
+++ src/main/java/frc/robot/commands/LiftExtendCommand.java
@@ -3,10 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Lift;
 
-public class LiftElevateCommand extends Command {
+public class LiftExtendCommand extends Command {
     private Lift lift = Lift.getInstance();
 
-    public LiftElevateCommand() {
+    public LiftExtendCommand() {
         addRequirements(lift);
     }
 

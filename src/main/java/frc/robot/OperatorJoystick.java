9b3cea14dffcd5347a9fbce8f2db93641db0cee5
Griffin M. Lamb <lambgm@1038.org>
2024-01-20 12:41:21 -0500

    updated operator joystick and deleted unused things

--- src/main/java/frc/robot/OperatorJoystick.java
+++ src/main/java/frc/robot/OperatorJoystick.java
@@ -3,30 +3,16 @@ package frc.robot;
 import frc.robot.libraries.XboxController1038;
 import frc.robot.constants.AcquisitionConstants;
 import frc.robot.constants.IOConstants;
-import frc.robot.subsystems.Acquisition;
-import frc.robot.subsystems.Lift;
 import frc.robot.subsystems.Scoring;
-import frc.robot.subsystems.Storage;
-import frc.robot.subsystems.SwagLights;
-import frc.robot.subsystems.Vision;
-import frc.robot.constants.StorageConstants;
 import frc.robot.constants.ScoringConstants;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Scoring.ElevatorSetpoints;
 import frc.robot.commands.AcquireCommand;
 import frc.robot.commands.LiftDepressCommand;
 import frc.robot.commands.LiftExtendCommand;
-import frc.robot.commands.ScoreNoteCommand;
+import frc.robot.commands.StorageRunCommand;
 
 public class OperatorJoystick extends XboxController1038 {
-    // Singleton Setup
-    private Acquisition acquisition = Acquisition.getInstance();
-    private Lift lift = Lift.getInstance();
     private Scoring scoring = Scoring.getInstance();
-    private Storage storage = Storage.getInstance();
-    private SwagLights swagLights = SwagLights.getInstance();
-    private Vision vision = Vision.getInstance();
 
     private static OperatorJoystick instance;
 
@@ -41,14 +27,10 @@ public class OperatorJoystick extends XboxController1038 {
     private OperatorJoystick() {
         super(IOConstants.kOperatorControllerPort);
 
-        aButton.whileTrue(new StorageRunCommand(StorageConstants.reverseMotorSpeed));
-        bButton.whileTrue(new StorageRunCommand(StorageConstants.motorSpeed));
+        aButton.whileTrue(new StorageRunCommand());
+        bButton.whileTrue(new StorageRunCommand());
         xButton.onTrue(new LiftDepressCommand());
         yButton.onTrue(new LiftExtendCommand());
-
-        D
-
-
 
         switch (this.getPOVPosition()) {
             case Up:
@@ -70,6 +52,5 @@ public class OperatorJoystick extends XboxController1038 {
         rightBumper.onTrue(new AcquireCommand(AcquisitionConstants.motorSpeed));
         rightTrigger.onTrue(new AcquireCommand(AcquisitionConstants.reverseMotorSpeed));
 
-        bu
     }
 }

1e84b7b1f3d530af09bb5972e97b7ad5db98c723
ChasingCode34 <rachitgupta1901@gmail.com>
2024-01-31 18:28:12 -0500

    Added a new command for running the scoring subsystem mechanisms backwards to shoot notes

---
+++ src/main/java/frc/robot/commands/ScoreNoteCommandRunningBackward.java
@@ -0,0 +1,52 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.ScoringConstants;
+import frc.robot.subsystems.Scoring;
+import frc.robot.subsystems.Scoring.ElevatorSetpoints;
+
+public class ScoreNoteCommandRunningBackward extends Command {
+    Scoring scoring = Scoring.getInstance();
+    private Timer timer = new Timer();
+    private double secondsToScore;
+    private ElevatorSetpoints elevatorSetpoint;
+
+    public ScoreNoteCommandRunningBackward(double secondsToScore, ElevatorSetpoints elevatorSetpoint) {
+        addRequirements(scoring);
+        this.secondsToScore = secondsToScore;
+        this.elevatorSetpoint = elevatorSetpoint;
+    }
+
+    public ScoreNoteCommandRunningBackward(ElevatorSetpoints elevatorSetpoint) {
+        addRequirements(scoring);
+        this.elevatorSetpoint = elevatorSetpoint;
+    }
+
+    @Override
+    public void initialize() {
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        if (elevatorSetpoint.equals(ElevatorSetpoints.amp)) {
+            scoring.setSetpoint(ElevatorSetpoints.amp.value);
+        }
+
+        // Waiting on methods for these
+        // scoring.runRollerBackwardToShoot(-ScoringConstants.rollerSpeed);
+        // scoring.runLoaderBackward(-ScoringConstants.loaderSpeed);
+
+    }
+
+    public boolean isFinished() {
+        return this.secondsToScore == 0 ? timer.get() > this.secondsToScore : false;
+    }
+
+    public void end() {
+        scoring.stopLoader();
+        scoring.stopRoller();
+    }
+
+}

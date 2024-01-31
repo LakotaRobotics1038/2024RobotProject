1788cbc95a5cce10aa4f8a9f40059b3068e23b32
Maryam A. Tunkara <tunkarama@1038.org>
2024-01-31 17:43:35 -0500

    we are become actuated

--- src/main/java/frc/robot/constants/LiftConstants.java
+++ src/main/java/frc/robot/constants/LiftConstants.java
@@ -4,5 +4,8 @@ public class LiftConstants {
     public static final int leftMotorPort = 0;
     public static final int rightMotorPort = 1;
 
+    public static final int leftServoPort = 0;
+    public static final int rightServoPort = 1;
+
     public static final double motorSpeed = 1;
 }
--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -5,11 +5,15 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Servo;
 import frc.robot.constants.LiftConstants;
 
 public final class Lift extends SubsystemBase {
     private CANSparkMax leftLiftMotor = new CANSparkMax(LiftConstants.leftMotorPort, MotorType.kBrushless);
     private CANSparkMax rightLiftMotor = new CANSparkMax(LiftConstants.rightMotorPort, MotorType.kBrushless);
+
+    private Servo leftServo = new Servo(LiftConstants.leftServoPort);
+    private Servo rightServo = new Servo(LiftConstants.rightServoPort);
 
     private static Lift instance;
 
@@ -27,8 +31,21 @@ public final class Lift extends SubsystemBase {
         rightLiftMotor.setIdleMode(IdleMode.kCoast);
         rightLiftMotor.follow(leftLiftMotor);
 
+        leftServo.setBoundsMicroseconds(2000, 1800, 1500, 1200, 1000);
+        rightServo.setBoundsMicroseconds(2000, 1800, 1500, 1200, 1000);
+
         leftLiftMotor.burnFlash();
         rightLiftMotor.burnFlash();
+    }
+
+    public void enableRatchets() {
+        leftServo.set(1);
+        rightServo.set(1);
+    }
+
+    public void disableRatchets() {
+        leftServo.set(0);
+        rightServo.set(0);
     }
 
     public void runPos() {

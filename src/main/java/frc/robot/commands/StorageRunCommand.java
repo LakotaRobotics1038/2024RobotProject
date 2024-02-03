5481fccaf4f784fcbeee5c75f88c0303c867bb72
Griffin M. Lamb <lambgm@1038.org>
2024-02-03 12:55:45 -0500

    made the is finsihed in storage run command to where it reutnrs true when the note has been seen oncef and then gone ip

--- src/main/java/frc/robot/commands/StorageRunCommand.java
+++ src/main/java/frc/robot/commands/StorageRunCommand.java
@@ -1,5 +1,40 @@
 package frc.robot.commands;
 
-public class StorageRunCommand {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Storage;
+
+public class StorageRunCommand extends Command {
+    private Storage storage = Storage.getInstance();
+
+    private boolean seenNote;
+    private double speed;
+
+    public StorageRunCommand() {
+        addRequirements(storage);
+    }
+
+    @Override
+    public void execute() {
+        storage.runLoader();
+        storage.runTransition();
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (storage.noteExitingStorage()) {
+            seenNote = true;
+            return false;
+        } else if (!storage.noteEnteringStorage() && seenNote) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        storage.stopStorage();
+        storage.stopTransition();
+    }
 
 }

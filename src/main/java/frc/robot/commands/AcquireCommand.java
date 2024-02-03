7158254db1679cfb5d401cab9171ca7eae93d67a
Myles A. Knowlin <knowlim2@1038.org>
2024-02-03 11:54:22 -0500

    Refactored AcquireCommand to use Acquisition Laser

--- src/main/java/frc/robot/commands/AcquireCommand.java
+++ src/main/java/frc/robot/commands/AcquireCommand.java
@@ -1,13 +1,29 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.AcquisitionConstants;
 import frc.robot.subsystems.Acquisition;
 
 public class AcquireCommand extends Command {
     private static Acquisition acquisition = Acquisition.getInstance();
+    private Timer timer = new Timer();
+    private double timeToAcquire;
 
     public AcquireCommand() {
         addRequirements(acquisition);
+        this.timeToAcquire = AcquisitionConstants.defaultTimeToAcquire;
+    }
+
+    public AcquireCommand(double timeToAcquire) {
+        addRequirements(acquisition);
+
+        this.timeToAcquire = timeToAcquire;
+    }
+
+    @Override
+    public void initialize() {
+        timer.start();
     }
 
     @Override
@@ -18,11 +34,14 @@ public class AcquireCommand extends Command {
     @Override
     public boolean isFinished() {
         // TODO: update with sensors
-        return false;
+
+        return acquisition.isNotePresent() || (timeToAcquire == 0) || (timer.get() > timeToAcquire);
     }
 
     @Override
     public void end(boolean interrupted) {
         acquisition.stop();
+        timer.stop();
+        timer.reset();
     }
 }

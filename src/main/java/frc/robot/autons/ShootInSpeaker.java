9853d45282d0992a196d9c7b7f4b7b8b85c270b1
Rachit Gupta <rachitgupta1901@gmail.com>
2024-02-10 15:07:52 -0500

    Created autonomous routine for scoring the speaker

---
+++ src/main/java/frc/robot/autons/ShootInSpeaker.java
@@ -0,0 +1,65 @@
+package frc.robot.autons;
+
+import java.nio.file.Path;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import frc.robot.commands.AcquireCommand;
+import frc.robot.commands.DriveVisionCommand;
+import frc.robot.commands.ScoringPositionCommand;
+import frc.robot.commands.ShootNoteCommand;
+import frc.robot.commands.StorageRunCommand;
+import frc.robot.subsystems.Scoring.ElevatorSetpoints;
+import frc.robot.subsystems.Vision.VisionTarget;
+
+public class ShootInSpeaker extends Auton {
+
+    public ShootInSpeaker(Alliance alliance) {
+        super(alliance);
+
+        PathPlannerPath startToShooting = PathPlannerPath.fromPathFile("From position 2 to speaker");
+        PathPlannerPath shootingToNoteTwo = PathPlannerPath.fromPathFile("From speaker to middle note");
+        PathPlannerPath noteTwoToShooting = PathPlannerPath.fromPathFile("From middle note to speaker");
+        PathPlannerPath shootingToNoteThree = PathPlannerPath.fromPathFile("From speaker to bottom note");
+        PathPlannerPath noteThreeToShooting = PathPlannerPath.fromPathFile("From bottom note to speaker");
+        PathPlannerPath shootingToMidline = PathPlannerPath.fromPathFile("From speaker to midline");
+
+        addCommands(
+                new ParallelRaceGroup(
+                        AutoBuilder.followPath(startToShooting),
+                        new DriveVisionCommand(VisionTarget.APT5)),
+                new ShootNoteCommand(),
+                new ParallelRaceGroup(
+                        AutoBuilder.followPath(shootingToNoteTwo),
+                        new DriveVisionCommand(VisionTarget.NOTES)),
+                new AcquireCommand(),
+                new ParallelCommandGroup(
+                        new ParallelRaceGroup(
+                                AutoBuilder.followPath(noteTwoToShooting),
+                                new DriveVisionCommand(VisionTarget.APT5)),
+                        new ScoringPositionCommand(ElevatorSetpoints.Trap)),
+                new StorageRunCommand(),
+                new ShootNoteCommand(),
+                new ParallelCommandGroup(
+                        new ParallelRaceGroup(
+                                AutoBuilder.followPath(shootingToNoteThree),
+                                new DriveVisionCommand(VisionTarget.NOTES)),
+                        new ScoringPositionCommand(ElevatorSetpoints.Ground)),
+                new AcquireCommand(),
+                new ParallelCommandGroup(
+                        new ParallelRaceGroup(
+                                AutoBuilder.followPath(noteThreeToShooting),
+                                new DriveVisionCommand(VisionTarget.APT5)),
+                        new ScoringPositionCommand(ElevatorSetpoints.Trap)),
+                new StorageRunCommand(),
+                new ShootNoteCommand(),
+                AutoBuilder.followPath(shootingToMidline)
+
+        );
+    }
+
+}

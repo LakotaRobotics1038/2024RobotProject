e4e70bb2e98a8985516175ab9e16389a830605f7
ChasingCode34 <rachitgupta1901@gmail.com>
2024-02-19 14:41:56 -0500

    Fixed Auton class

--- src/main/java/frc/robot/autons/Auton.java
+++ src/main/java/frc/robot/autons/Auton.java
@@ -1,14 +1,47 @@
 package frc.robot.autons;
 
+import java.util.Optional;
+
+import com.pathplanner.lib.commands.FollowPathHolonomic;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.PathPlannerTrajectory;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.constants.FieldConstants;
 import frc.robot.subsystems.DriveTrain;
 
 public abstract class Auton extends SequentialCommandGroup {
+    private Pose2d initialPose;
     protected DriveTrain driveTrain = DriveTrain.getInstance();
     protected Alliance alliance;
 
     public Auton(Alliance alliance) {
         this.alliance = alliance;
+    }
+
+    protected void setInitialPose(PathPlannerTrajectory initialTrajectory) {
+        this.initialPose = initialTrajectory.getInitialTargetHolonomicPose();
+
+        // We need to invert the starting pose for the red alliance.
+        if (alliance == Alliance.Red) {
+            Translation2d transformedTranslation = new Translation2d(this.initialPose.getX(),
+                    FieldConstants.kFieldWidth - this.initialPose.getY());
+            Rotation2d transformedHeading = this.initialPose.getRotation().times(-1);
+
+            this.initialPose = new Pose2d(transformedTranslation, transformedHeading);
+        }
+    }
+
+    public Pose2d getInitialPose() {
+        return initialPose;
     }
 }

4d20a752530aac16ddec4749a9f59a59979c11d1
Griffin Lamb <115355881+Griffin9881@users.noreply.github.com>
2024-02-24 09:30:46 -0500

    Update StorageConstants.java

--- src/main/java/frc/robot/constants/StorageConstants.java
+++ src/main/java/frc/robot/constants/StorageConstants.java
@@ -6,8 +6,9 @@ public class StorageConstants {
     public static final int leftStoragePort = 11;
     public static final int rightStoragePort = 8;
 
-    public static final int enterStorageLaserPort = 0;
-    public static final int exitStorageLaserPort = 0;
+    //TODO: placeholder ports
+    public static final int enterStorageLaserPort = 25;
+    public static final int exitStorageLaserPort = 26;
 
     public static final double transitionSpeed = 0.0;
     public static final double reverseTransitionSpeed = -1;

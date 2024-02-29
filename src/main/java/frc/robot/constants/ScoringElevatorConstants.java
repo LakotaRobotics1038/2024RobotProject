e0f90a27f1484cadae7918642ce7a47d8b4e92b6
Griffin Lamb <115355881+Griffin9881@users.noreply.github.com>
2024-02-29 15:25:56 -0500

    Changed setpoint for trap from 14 to 15

--- src/main/java/frc/robot/constants/ScoringElevatorConstants.java
+++ src/main/java/frc/robot/constants/ScoringElevatorConstants.java
@@ -10,7 +10,7 @@ public class ScoringElevatorConstants {
 
     public static final double groundSetpoint = 0;
     public static final double ampSetpoint = 4.0;
-    public static final double trapSetpoint = 14.0;
+    public static final double trapSetpoint = 15.0;
 
     public static final double maxDistance = 0;
     public static final double minDistance = maxElevatorInches;

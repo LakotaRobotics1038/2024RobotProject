bd02fa0e2b2c8f937283fd622dfa98bf696285d7
Myles A. Knowlin <knowlim2@1038.org>
2024-03-02 11:40:24 -0500

    Made TryOut Changes

--- src/main/java/frc/robot/commands/FullAcquireSequenceCommand.java
+++ src/main/java/frc/robot/commands/FullAcquireSequenceCommand.java
@@ -2,11 +2,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Acquisition;
+import frc.robot.subsystems.ScoringElevator;
 import frc.robot.subsystems.Storage;
+import frc.robot.subsystems.ScoringElevator.ElevatorSetpoints;
 
 public class FullAcquireSequenceCommand extends Command {
     private Storage storage = Storage.getInstance();
     private Acquisition acquisition = Acquisition.getInstance();
+    private ScoringElevator scoringElevator = ScoringElevator.getInstance();
 
     public FullAcquireSequenceCommand() {
         addRequirements(storage, acquisition);
@@ -14,10 +17,12 @@ public class FullAcquireSequenceCommand extends Command {
 
     @Override
     public void execute() {
-        acquisition.acquire();
-        acquisition.runSushi();
-        storage.runTransition();
-        storage.runStorage();
+        if (scoringElevator.getSetpoint() == ElevatorSetpoints.Ground.value) {
+            acquisition.acquire();
+            acquisition.runSushi();
+            storage.runTransition();
+            storage.runStorage();
+        }
     }
 
     @Override
@@ -26,7 +31,7 @@ public class FullAcquireSequenceCommand extends Command {
     }
 
     @Override
-    public void end(boolean isFinished) {
+    public void end(boolean interrupted) {
         acquisition.stopIntake();
         acquisition.stopSushi();
         storage.stopTransition();

59fe54ac2f2951510eaead82e223182f21b9fc91
1038 Pit <1038Pit@1038-PIT-L1.ad.lakotarobotics.org>
2024-03-07 11:37:39 -0500

    updated scoring motor speed

--- src/main/java/frc/robot/constants/ScoringConstants.java
+++ src/main/java/frc/robot/constants/ScoringConstants.java
@@ -4,5 +4,5 @@ public class ScoringConstants {
     public static final int rollerMotorPort = 12;
     public static final double shootSpeakerSpeed = 1.0;
     public static final double feedApmSpeed = -0.25;
-    public static final double scoreAmpSpeed = 0.25;
+    public static final double scoreAmpSpeed = 0.75;
 }

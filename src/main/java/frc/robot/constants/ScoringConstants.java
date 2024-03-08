4eeb4d29f22cda44344115b7163f74eb832d7aa4
1038 Pit <1038Pit@1038-PIT-L1.ad.lakotarobotics.org>
2024-03-08 15:37:17 -0500

    increase trap speed

--- src/main/java/frc/robot/constants/ScoringConstants.java
+++ src/main/java/frc/robot/constants/ScoringConstants.java
@@ -3,7 +3,7 @@ package frc.robot.constants;
 public class ScoringConstants {
     public static final int rollerMotorPort = 12;
     public static final double feedTrapSpeed = -0.25;
-    public static final double scoreTrapSpeed = 0.75;
+    public static final double scoreTrapSpeed = 1.0;
     public static final double feedApmSpeed = -0.25;
     public static final double scoreAmpSpeed = 0.75;
 

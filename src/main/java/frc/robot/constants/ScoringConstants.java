00c5838a6128cc4a2ac60aef5e1044437ca19567
1038 Pit <1038Pit@1038-PIT-L1.ad.lakotarobotics.org>
2024-03-09 13:05:03 -0500

    changed motor speed

--- src/main/java/frc/robot/constants/ScoringConstants.java
+++ src/main/java/frc/robot/constants/ScoringConstants.java
@@ -2,9 +2,9 @@ package frc.robot.constants;
 
 public class ScoringConstants {
     public static final int rollerMotorPort = 12;
-    public static final double feedTrapSpeed = -0.25;
+    public static final double feedTrapSpeed = -0.3;
     public static final double scoreTrapSpeed = 1.0;
-    public static final double feedApmSpeed = -0.25;
+    public static final double feedApmSpeed = -0.3;
     public static final double scoreAmpSpeed = 0.75;
 
     public enum ScoringLocation {

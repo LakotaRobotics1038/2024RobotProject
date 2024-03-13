c6bcf35e18e4a610a401c9960f2f0fb07f540d26
Griffin Lamb <115355881+Griffin9881@users.noreply.github.com>
2024-03-13 10:16:43 -0400

    fixed build fail hopefully

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -115,7 +115,7 @@ public final class Lift extends SubsystemBase {
      */
     public void runLeftUp() {
         if (this.leftRatchetUnlocked()) {
-            if (leftLiftEncoder.getPosition() < LiftConstants.maxExtension()) {
+            if (leftLiftEncoder.getPosition() < LiftConstants.maxExtension) {
                 leftLiftMotor.set(LiftConstants.motorSpeed);
             } else {
                 leftLiftMotor.stopMotor();
@@ -130,7 +130,7 @@ public final class Lift extends SubsystemBase {
      */
     public void runRightUp() {
         if (this.rightRatchetUnlocked()) {
-            if (rightLiftEncoder.getPosition() < LiftConstants.maxExtension()) {
+            if (rightLiftEncoder.getPosition() < LiftConstants.maxExtension) {
                 rightLiftMotor.set(LiftConstants.motorSpeed);
             } else {
                 rightLiftMotor.stopMotor();

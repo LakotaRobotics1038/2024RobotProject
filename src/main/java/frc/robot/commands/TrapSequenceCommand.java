9b1fdca657bc3e28fe55658678dab27bc4da5b4c
Griffin <lamb.griffin@icloud.com>
2024-03-14 19:03:28 -0400

    fixed constructor

--- src/main/java/frc/robot/commands/TrapSequenceCommand.java
+++ src/main/java/frc/robot/commands/TrapSequenceCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.ScoringElevatorPositionCommand.FinishActions;
@@ -9,15 +8,13 @@ import frc.robot.subsystems.ScoringElevator.ElevatorSetpoints;
 
 public class TrapSequenceCommand extends SequentialCommandGroup {
 
-    public void TrapSequenceCommand() {
+    public TrapSequenceCommand() {
         this.addCommands(
-                new ParallelCommandGroup(
-                        new DrawbridgeDownCommand(),
-                        new SequentialCommandGroup(
-                                new WaitCommand(3),
-                                new ScoringElevatorPositionCommand(ElevatorSetpoints.Trap, FinishActions.NoDisable),
-                                new ScoreNoteCommand(ScoringLocation.Trap, 3),
-                                new ScoringElevatorPositionCommand(ElevatorSetpoints.Ground,
-                                        FinishActions.NoDisable))));
+                new DrawbridgeDownCommand(),
+                new WaitCommand(3),
+                new ScoringElevatorPositionCommand(ElevatorSetpoints.Trap, FinishActions.NoDisable),
+                new ScoreNoteCommand(ScoringLocation.Trap, 3),
+                new ScoringElevatorPositionCommand(ElevatorSetpoints.Ground,
+                        FinishActions.NoDisable));
     }
 }

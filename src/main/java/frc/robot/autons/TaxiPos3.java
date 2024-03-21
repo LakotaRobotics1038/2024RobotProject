0536645aa91907c814822f4b0c717e69fcafd530
Griffin <lamb.griffin@icloud.com>
2024-03-20 20:04:33 -0400

    got rid of paths used for practice

--- src/main/java/frc/robot/autons/TaxiPos3.java
+++ src/main/java/frc/robot/autons/TaxiPos3.java
@@ -2,9 +2,7 @@ package frc.robot.autons;
 
 import java.util.Optional;
 
-import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Dashboard;
 
 public class TaxiPos3 extends Auton {
@@ -12,14 +10,9 @@ public class TaxiPos3 extends Auton {
         super(alliance);
 
         Dashboard.getInstance().setTrajectory(Trajectories.getFromPos3TaxiTrajectory());
-        Dashboard.getInstance().setTrajectory(Trajectories.getFromTaxi3ToPos1Trajectory());
         this.setInitialPose(Trajectories.getFromPos3TaxiTrajectory());
 
         addCommands(
-                followPathCommand(Paths.taxiPath3),
-                new WaitCommand(0.5),
-                followPathCommand(Paths.pathFromTaxi3ToPos1),
-                new WaitCommand(0.5),
                 followPathCommand(Paths.taxiPath3));
     }
 }

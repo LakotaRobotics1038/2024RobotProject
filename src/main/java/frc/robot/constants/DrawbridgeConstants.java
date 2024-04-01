48832da6746b717fd9bce23ab83ca623ca726fc4
Griffin <lamb.griffin@icloud.com>
2024-04-01 19:52:26 -0400

    drawbridge constants

--- src/main/java/frc/robot/constants/DrawbridgeConstants.java
+++ src/main/java/frc/robot/constants/DrawbridgeConstants.java
@@ -6,7 +6,7 @@ public class DrawbridgeConstants {
     public static final int rightServoPort = 3;
 
     public static final double minLeftDrawbridgeExtension = 0.0;
-    public static final double maxLeftDrawbridgeExtension = 0.3;
-    public static final double minRightDrawbridgeExtension = 0.08;
-    public static final double maxRightDrawbridgeExtension = 0.45;
+    public static final double maxLeftDrawbridgeExtension = 0.65;
+    public static final double minRightDrawbridgeExtension = 0.0;
+    public static final double maxRightDrawbridgeExtension = 0.6;
 }

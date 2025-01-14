b12392675d727e9af9ed702b78588e8d9dc094e6
Wesley Reed <reed.wesley.s@gmail.com>
2025-01-14 16:43:23 -0500

    remove unused commands

--- src/main/java/frc/robot/commands/FeedNoteCommand.java
+++ src/main/java/frc/robot/commands/FeedNoteCommand.java
@@ -1,30 +1,16 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.ScoringConstants.ScoringLocation;
 import frc.robot.subsystems.Scoring;
 
 public class FeedNoteCommand extends Command {
     private Scoring scoring = Scoring.getInstance();
-    private Timer timer = new Timer();
-    private double secondsToScore = 0;
     private ScoringLocation scoringLoc;
-
-    public FeedNoteCommand(ScoringLocation scoringLoc, double secondsToScore) {
-        this.addRequirements(scoring);
-        this.scoringLoc = scoringLoc;
-        this.secondsToScore = secondsToScore;
-    }
 
     public FeedNoteCommand(ScoringLocation scoringLoc) {
         this.addRequirements(scoring);
         this.scoringLoc = scoringLoc;
-    }
-
-    @Override
-    public void initialize() {
-        timer.start();
     }
 
     @Override
@@ -41,13 +27,11 @@ public class FeedNoteCommand extends Command {
 
     @Override
     public boolean isFinished() {
-        return this.secondsToScore != 0 ? timer.get() > this.secondsToScore : false;
+        return false;
     }
 
     @Override
     public void end(boolean interrupted) {
         scoring.stop();
-        timer.stop();
-        timer.reset();
     }
 }

5a4b9e507f9d919a67f893ce6bec8aacdc39f03f
Wesley Reed <reed.wesley.s@gmail.com>
2025-01-14 19:17:57 -0500

    make final

--- src/main/java/frc/robot/constants/AutoPaths.java
+++ src/main/java/frc/robot/constants/AutoPaths.java
@@ -1,21 +1,21 @@
 package frc.robot.constants;
 
 public class AutoPaths {
-    public static String pathFromPosition1ToAmp = "From position 1 to amp";
+    public static final String pathFromPosition1ToAmp = "From position 1 to amp";
 
-    public static String pathFromAmpToNote1 = "From amp to top note";
+    public static final String pathFromAmpToNote1 = "From amp to top note";
 
-    public static String pathFromNote1ToAmp = "From note 1 to amp";
+    public static final String pathFromNote1ToAmp = "From note 1 to amp";
 
-    public static String pathFromAmpToMidline = "From amp to midline";
-    public static String pathFromAmpToMidlineAcquire = "From amp to midline acquire";
-    public static String pathFromMidlineNoteToAmp = "From midline note to amp";
-    public static String pathFromAmpToMidlineNote2 = "From amp to midline acquire Note2";
-    public static String pathFromNote2ToTrap = "From Note 2 to Mid Trap";
+    public static final String pathFromAmpToMidline = "From amp to midline";
+    public static final String pathFromAmpToMidlineAcquire = "From amp to midline acquire";
+    public static final String pathFromMidlineNoteToAmp = "From midline note to amp";
+    public static final String pathFromAmpToMidlineNote2 = "From amp to midline acquire Note2";
+    public static final String pathFromNote2ToTrap = "From Note 2 to Mid Trap";
 
-    public static String pathFromAmpToBack = "from amp to back";
+    public static final String pathFromAmpToBack = "from amp to back";
 
-    public static String taxiPath1 = "Taxi Position 1";
-    public static String taxiPath2 = "Taxi Position 2";
-    public static String taxiPath3 = "Taxi Position 3";
+    public static final String taxiPath1 = "Taxi Position 1";
+    public static final String taxiPath2 = "Taxi Position 2";
+    public static final String taxiPath3 = "Taxi Position 3";
 }
